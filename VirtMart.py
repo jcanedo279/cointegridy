0e3f42b5db443294e0d6111b1a8b90a79f342388
Jorge Canedo <jcanedo@g.hmc.edu>
2021-10-31 22:33:55 -0700

    some last changes, seperation of data

---
+++ VirtMart.py
@@ -0,0 +1,26 @@
+
+
+
+class VirtMark:
+    
+    def __init__(self, init_investment=1000):
+        
+        
+        self.investment = init_investment
+        
+        self.assets = {
+            
+        }
+        
+        pass
+    
+    def buy(self, )
+
+
+
+def main():
+    marty = VirtMark()
+
+
+if __name__ == "__main__":
+    main()
