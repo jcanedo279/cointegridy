9150d73a734f0e27beaff5392d6c8fa071f85a69
Jorge Canedo <jcanedo@g.hmc.edu>
2021-12-03 16:44:26 -0800

    recent changes

--- cointegridy/src/classes/data_loader.py
+++ cointegridy/src/classes/data_loader.py
@@ -16,7 +16,7 @@ from cointegridy.src.utils.stats import sharpe_ratio
 TXT_DEL=' '
 CSV_DEL=','
 
-ROOT = ''
+ROOT = os.getcwd().replace('/cointegridy', '/')
 
 if not os.path.exists('data/'):
     os.mkdir('data/')
@@ -408,52 +408,6 @@ class DataLoader:
                     
                     writer.writerows([[d['open_tmsp'], d['open']] for d in id_response])
 
-
-if __name__=='__main__':
-    
-    
-    #################################
-    ## PULL STATIC HISTORICAL DATA ##
-    #################################
-    
-    VALID_INTERVALS = ['1m','3m','5m','15m','30m','1h','2h','4h','6h','8h','12h','1d','3d','1w','1M']
-    
-    interval_flag = '5m'
-    
-    db_start_date, db_end_date = (2021,1,28), (2021,2,1)
-    db_start_Time, db_end_Time = Time.date_to_Time(*db_start_date), Time.date_to_Time(*db_end_date)
-    
-    ## UNCOMMENT TO UPDATE SHARPE RATIOS
-    # pull_sharpe_ratios(start_Time=db_start_Time, end_Time=db_end_Time, interval_flag=interval_flag)
-    
-    ## UNCOMMENT TO 
-    # num_symbols = 100
-    # crypto_ids = get_best_stocks_from_CSV(SR_PATH, CSV_DEL, num_symbols)
-    
-    ## UNCOMMENT TO UPDATE HISTORICAL DATA
-    # DataLoader.pull_data(start_Time=db_start_Time, end_Time=db_end_Time, interval_flag=interval_flag, bnc_ids=crypto_ids)
-    
-    # data_loader = DataLoader()
-    
-    ## GET AVAILABLE IDS FROM DRIVE
-    # ids = data_loader.get_ids()
-    # print(ids)
-
-    
-    
-    ###################################
-    ## PULL DYNAMMIC HISTORICAL DATA ##
-    ###################################
-    
-    start_date, end_date = (2021,1,28,9,30), (2021,2,1,10,0)
-    start_Time, end_Time = Time.date_to_Time(*start_date), Time.date_to_Time(*end_date)
-    
-    sample_symbol = 'BTCUSDT'
-    
-    tree_loader = TreeLoader()
-    
-    for datum in tree_loader[sample_symbol][start_Time:end_Time]:
-        print(datum)
     
     
     
--- cointegridy/tests/test_cointegridy.py
+++ cointegridy/tests/test_cointegridy.py
@@ -1,18 +1,18 @@
 import os
 
 
-ROOT = os.getcwd()
+ROOT = os.getcwd() + '/cointegridy'
 
-CLASSES_PATH = '/cointegridy/src/classes/'
-UTILS_PATH = '/cointegridy/src/utils/'
-SCRIPTS_PATH = '/cointegridy/scripts/'
-EXAMPLES_PATH = '/cointegridy/examples/'
+CLASSES_PATH = '/src/classes/'
+UTILS_PATH = '/src/utils/'
+SCRIPTS_PATH = '/scripts/data/'
+EXAMPLES_PATH = '/examples/'
 
 INVALID_FILENAMES = {'__pycache__', '.DS_Store'}
 
 def test_cointegridy_compiles():
     for _dir in [CLASSES_PATH, UTILS_PATH, SCRIPTS_PATH, EXAMPLES_PATH]:
-
+        print(ROOT+_dir)
         for filename in os.listdir(ROOT+_dir):
             if filename in INVALID_FILENAMES: continue
             
